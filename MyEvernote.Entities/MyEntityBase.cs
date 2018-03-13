d2642dd63d25bdb3bffe19e7a6b9f0f511ab0860
ozitron <ozankomurcu@gmail.com>
2018-03-13 11:02:11 +0300

    major changes in class libraries

--- MyEvernote.Entities/MyEntityBase.cs
+++ MyEvernote.Entities/MyEntityBase.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +10,16 @@ namespace MyEvernote.Entities
 {
     public class MyEntityBase
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]   //primary key, id auto increase
         public int Id { get; set; }
 
+        [Required]
         public DateTime CreatedOn { get; set; }
+
+        [Required]
         public DateTime ModifiedOn { get; set; }
+
+        [Required, StringLength(30)]
         public string ModifiedUserName { get; set; }
     }
 }
